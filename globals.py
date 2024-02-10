cbf82b965f78c9b730ec49bbe9dc43112f773ca3
Nathan Vuong <ncvuong@Nathans-MacBook-Air.local>
2024-02-10 01:13:56 -0800

    Changed stopwords file path

--- globals.py
+++ globals.py
@@ -8,13 +8,13 @@ icsUciEdu = dict()
 def report():
     filePath = "report.txt"
     with open(filePath, "w") as file:
-        file.write("Unique pages found: " + str(len(uniquePages)))
-        file.write("Longest page: " + longestPage[0])
-        file.write("Length of longest page: " + str(longestPage[1]))
+        file.write("Unique pages found: " + str(len(uniquePages)) + "\n")
+        file.write("Longest page: " + longestPage[0] + "\n")
+        file.write("Length of longest page: " + str(longestPage[1]) + "\n")
         sortedFrequency = getFreq(totalWordFrequency)
         for index, word in enumerate(sortedFrequency):
-            file.write("#" + str(index) + " most common word w/ frequency: " + word[0] + ", " + str(word[1]))
-        file.write("Subdomains in ics.uci.edu domain: " + str(len(icsUciEdu)))
+            file.write("#" + str(index) + " most common word w/ frequency: " + word[0] + ", " + str(word[1]) + "\n")
+        file.write("Subdomains in ics.uci.edu domain: " + str(len(icsUciEdu)) + "\n")
         sortedSubdomain = dict(sorted(icsUciEdu.items()))
         for key in sortedSubdomain:
-            file.write("Subdomains and their unique pages: " + key + ", " + str(sortedSubdomain[key]))
+            file.write("Subdomains and their unique pages: " + key + ", " + str(sortedSubdomain[key]) + "\n")
--- tokenizer/PartA.py
+++ tokenizer/PartA.py
@@ -41,7 +41,7 @@ def tokenizeString(words: str) -> list:
 
 def removeStopwords(lst: list) -> list:
     newList = lst.copy()
-    filePath = "stopwords.txt"
+    filePath = "tokenizer/stopwords.txt"
     stopLst = tokenize(filePath)
     for token in lst:
         if token in stopLst:
